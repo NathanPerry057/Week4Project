73193bf89983e21e47ffa3e1f18554647182e231
NathanPerry057 <G00394355@atu.ie>
2023-10-16 22:19:13 +0100

    Added dependencies, annotations, main and userdetails class and the registration interface

---
+++ src/test/java/ie/atu/week4project/Week4ProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package ie.atu.week4project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Week4ProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
